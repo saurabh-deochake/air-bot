d06e83786872248511b0bf8a89de07edfcd964c4
saurabh-deochake <saurabh.d04@gmail.com>
2016-05-13 18:26:09 -0400

    added stream listener

--- app/airbot.py
+++ app/airbot.py
@@ -30,32 +30,61 @@ import __future__
 import logging
 import tweepy
 from ConfigParser import SafeConfigParser
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
 
 CONFIG_FILE = "/etc/airbot.conf"
 
-class airbot():
-	def auth(self):
-   		parser = SafeConfigParser()
-		parser.read(CONFIG_FILE)
 
-		ckey = parser.get('twitter','ckey')
-		csecret = parser.get('twitter','csecret')
-		atoken = parser.get('twitter','atoken')
-		asecret = parser.get('twitter', 'asecret')
+def auth():
+	parser = SafeConfigParser()
+	parser.read(CONFIG_FILE)
 
-   		# Create authentication token using our details
-   		auth = tweepy.OAuthHandler(ckey, csecret)
-   		auth.set_access_token(atoken, asecret)
-   		self.update(auth)
+	ckey = parser.get('twitter','ckey')
+	csecret = parser.get('twitter','csecret')
+	atoken = parser.get('twitter','atoken')
+	asecret = parser.get('twitter', 'asecret')
 
-	def update(self, auth):
+	# Create authentication token using our details
+	auth = tweepy.OAuthHandler(ckey, csecret)
+	auth.set_access_token(atoken, asecret)
+	api = tweepy.API(auth)
+	twitterStream = Stream(auth, streamer())	
+	twitterStream.filter(track=["#airqualityin"]) 
 
-		api = tweepy.API(auth)
-		api.update_status('Airbot says hi! It\'s %s' % (time.strftime("%H:%M:%S")))
 
-if __name__ == "__main__":
+class streamer(StreamListener):
+	def on_data(self, data):
+		try:
+			userName = data.split(',"screen_name":"')[1].split('","location')[0]
+			tweet = data.split(',"text":"')[1].split('","source')[0]
+			print tweet[tweet.index("#airqualityin")+1:]
+
+			#print data
+
+			fetchedTweet = "@"+userName+"-"+tweet
+			#print fetchedTweet
+			time.sleep(5)
+			return True
+
+		except BaseException, e:
+			print 'Failed Ondata,', str(e)
+			time.sleep(5)
+		except KeyboardInterrupt, k:
+			print 'Keyboard Interrupt Occured,',str(k)
+			quit()
+
+	def on_error(self, status):
+		print status
+
+	'''
+	def update(self, api):
+		print "update"
+		#api.update_status('Airbot says hi! It\'s %s' % (time.strftime("%H:%M:%S")))
+	'''
 	
-	bot = airbot()
-	bot.auth()
+if __name__ == "__main__":
+	auth()
 
 
