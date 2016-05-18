cf1449f630797033e7615da8c97c7551d383ee6b
saurabh-deochake <saurabh.d04@gmail.com>
2016-05-18 18:35:37 -0400

    API error codes 20 and 21 handled

--- app/airbot.py
+++ app/airbot.py
@@ -40,6 +40,9 @@ from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 
 CONFIG_FILE = "/etc/airbot.conf"
+ERROR_TWEET = "@%s, to get air quality in your city, please tweet \"#airqualityin followed by city name (US only).\""
+UNSUPPORTED_ERROR = "@%s, Provided location is not yet supported! Please enter US city name!"
+NODATAAVAILABLE_ERROR = "@%s, Air quality data is not available currently. Please check back in some time!"
 
 parser = SafeConfigParser()
 parser.read(CONFIG_FILE)
@@ -71,10 +74,12 @@ class streamer(StreamListener):
 			print tweet
 			print city
 			latitude, longitude = self.get_latlon(city)
-			print latitude
-			print longitude
-			aqi, quality = self.get_aqi(latitude, longitude)
-			self.on_update(userName, city, aqi, quality)
+			
+			if latitude != -1 and longitude != -1:
+				print latitude
+				print longitude
+				aqi, quality = self.get_aqi(latitude, longitude)
+				self.on_update(userName, city, aqi, quality)
 			# 1) get lattitude and longitude from the city
 			# 2) pass the lattitude and longitude to get_aqi
 
@@ -92,37 +97,68 @@ class streamer(StreamListener):
 
 	def on_error(self, status):
 		print status
-
+		return False
 	
 	def on_update(self, userName, city, aqi, quality):
 		print "update"
-		print '@'+userName+' ,Current Air Quality in '+city+' is '+str(aqi)+ ' !'
-		api.update_status('@'+userName+', Air Quality in '+city+' is '+ quality.split()[0].lower()+ ' with index '+str(aqi)+ '. (via @BreezoMeter)')
+
+		if aqi == -1 and quality == UNSUPPORTED_ERROR:
+			api.update_status(UNSUPPORTED_ERROR % (userName))
+		elif aqi == -1 and quality == NODATAAVAILABLE_ERROR:
+			api.update_status(NODATAAVAILABLE_ERROR % (userName))
+		else:
+
+			print '@'+userName+' ,Current Air Quality in '+city+' is '+str(aqi)+ ' !'
+			api.update_status('@'+userName+', Air Quality in '+city+' is '+ quality.split()[0].lower()+ ' with index '+str(aqi)+ '. (via @BreezoMeter)')
+
+	def on_limit(self, track):
+		print track
+
+	def on_timeout(self):
+		print "Timeout, sleeping for 60 seconds...\n"
+		time.sleep(60)
+		return 
 	
 	def get_latlon(self, city):
-		geolocator = Nominatim()
-		location = geolocator.geocode(city)
-		latitude, longitude = location.latitude, location.longitude
-		return latitude, longitude
+		try:
+			geolocator = Nominatim()
+			location = geolocator.geocode(city)
+			latitude, longitude = location.latitude, location.longitude
+			return latitude, longitude
+		except: 
+			return -1, -1
+
 
 	def get_aqi(self, latitude, longitude):
-		parser = SafeConfigParser()
-		parser.read(CONFIG_FILE)
-		akey = parser.get('breezometer', 'akey')
-		context = ssl._create_unverified_context()
+		
+		try:
+			parser = SafeConfigParser()
+			parser.read(CONFIG_FILE)
+			akey = parser.get('breezometer', 'akey')
+		except FileNotFoundError, e:
+			print "Config file not found! Aborting..."
+			print "Trace: ",str(e)
 
-		optionsUrl = 'http://api.breezometer.com/baqi/?lat='+str(latitude)+'&lon='+str(longitude)+'&key='+akey
+		try:
+			context = ssl._create_unverified_context()
 
-		print optionsUrl
+			optionsUrl = 'http://api.breezometer.com/baqi/?lat='+str(latitude)+'&lon='+str(longitude)+'&key='+akey
 
-		#+akey
-		optionsPage = urlopen(optionsUrl, context=context).read()
-		aqi_json = json.loads(optionsPage)
-		#if aqi_json['error']:
-			#print "Data not available!"
-		#else:
+			print optionsUrl
 
-		return aqi_json['breezometer_aqi'], aqi_json['breezometer_description']
+			optionsPage = urlopen(optionsUrl, context=context).read()
+			aqi_json = json.loads(optionsPage)
+			
+			print aqi_json['error']['code']
+
+			if aqi_json['error']['code'] == 20: 
+				return -1, UNSUPPORTED_ERROR
+			elif aqi_json['error']['code'] == 21:
+				return -1, NODATAAVAILABLE_ERROR
+			else:
+				return aqi_json['breezometer_aqi'], aqi_json['breezometer_description']
+		except BaseException, e:
+			print str(e)
 
 if __name__ == "__main__":
 	stream = streamer()
